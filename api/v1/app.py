7d806b804932f8b82ca7198fca36fdd2b90f62e7
Ermi24et <lilermi24@gmail.com>
2024-01-08 18:00:57 +0300

    updating api

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
-def teardown(self):
+def teardown(error):
     """ closes an engine """
     from models import storage
     storage.close()
