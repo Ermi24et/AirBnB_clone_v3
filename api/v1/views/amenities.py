c0bf5782662f30a3f1a30725ca12fc96fe3636fe
Ermi24et <lilermi24@gmail.com>
2024-01-08 23:28:45 +0300

    updated amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -5,6 +5,7 @@ a new view for State objects that handles all default RESTFul API actions
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
+from models.amenity import Amenity
 from models.state import State
 
 
