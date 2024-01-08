a2990587cc8cd4b254ec723ca5a72f2f6d8b30e7
Ermi24et <lilermi24@gmail.com>
2024-01-08 17:52:31 +0300

    added api directory

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" a script to start an api """
+from flask import Flask
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown(self):
+    """ closes an engine """
+    from models import storage
+    storage.close()
+
+
+if __name__ == "__main__":
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
