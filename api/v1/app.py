3ae781e2535f4e7298890f64de93894a71c08a86
Ermi24et <lilermi24@gmail.com>
2024-01-09 22:49:13 +0300

    updated app file to add corps

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ a script to start an api """
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
